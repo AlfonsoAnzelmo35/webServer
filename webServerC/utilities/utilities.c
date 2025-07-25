74109e12a797a561bd90289acd31b6c199a65004
Alfonso Anzelmo <anzelmo35@gmail.com>
2025-07-25 10:12:02 +0200

    correzione risposta404

--- webServerC/utilities/utilities.c
+++ webServerC/utilities/utilities.c
@@ -11,8 +11,8 @@
 #include "base64.h"
 
 
-char img_for_resource_not_found[]= "/home/alfonso/webServerC/immagini/Grump_GattoIncazzato.jpg" ;
-char html_for_resource_not_found[] = "/home/alfonso/webServerC/htmls/resource_not_found.html" ;
+char img_for_resource_not_found[]= "/home/alfonso/webserver_git_C/webServerC/immagini/Grump_GattoIncazzato.jpg" ;
+char html_for_resource_not_found[] = "/home/alfonso/webserver_git_C/webServerC/htmls/resource_not_found.html" ;
 
 struct request_file{
 	char file[100];
@@ -95,157 +95,47 @@ void get_file_fileExtension2(struct request_file *requestfile, char * requested_
 	strcpy(requestfile->ext, token);
 }
 
+char *get_base64Img(const char* path) {
+    int fd = open(path, O_RDONLY);
+    if (fd < 0) {
+        perror("Errore apertura file immagine");
+        exit(EXIT_FAILURE);
+    }
 
+    // Ottieni la dimensione del file
+    off_t file_size = lseek(fd, 0, SEEK_END);
+    lseek(fd, 0, SEEK_SET);  // ritorna all'inizio
 
-void file_not_found_send_html(int fd_client_socket){
-	printf("\n invio html\n");
-	struct stat st;
-        char buf[256] ;
-        stat(html_for_resource_not_found, &st) ;
-	
-	snprintf(buf, 256,
-                "HTTP/1.1 404 NOT FOUND\r\n"
-                "Content-Type: text/html; charset=utf-8\r\n"
-                "Content-Length: %ld\r\n"
-                "Connection: keep-alive\r\n"
-                "Cache-Control: no-cache\r\n"
-                "Date: Sun, 14 Jul 2025 10:23:45 GMT\r\n"
-                "Server: Apache/2.4.41 (Ubuntu)\r\n"
-                "\r\n", st.st_size);
-        send(fd_client_socket, buf, strlen(buf) , 0) ;
+    // Alloca il buffer dinamico per contenere tutti i byte dell'immagine
+    unsigned char *buffer = malloc(file_size);
+    if (!buffer) {
+        perror("Errore malloc");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
 
+    // Legge il contenuto dell'immagine
+    ssize_t total_read = read(fd, buffer, file_size);
+    close(fd);
 
-	//apertura del file html 	
-	int fd_HTML = 0 ;
-       	if((fd_HTML = open(html_for_resource_not_found, O_RDONLY))< 0){
-		fprintf(stderr, "Errore %d: %s\n", errno, strerror(errno)) ;
-                exit(EXIT_FAILURE);
-	}
+    if (total_read != file_size) {
+        fprintf(stderr, "Errore nella lettura completa del file\n");
+        free(buffer);
+        exit(EXIT_FAILURE);
+    }
 
-	char html_buf[4096];	
-	int tot_byte_read = 0;
-        int byte_read = 0;
- 	while((byte_read = read(fd_HTML, html_buf + tot_byte_read, sizeof(html_buf))) > 0) {
-            	
-                if(byte_read < 0){
-                	fprintf(stderr, "Errore %d: %s\n", errno, strerror(errno)) ;
-                        exit(EXIT_FAILURE);
-                }
+    // Codifica in base64
+    size_t output_len = 0;
+    char *encoded = base64_encode(buffer, file_size, &output_len);
 
-		ssize_t sent = 0;
-                while (sent < byte_read) {
-                        ssize_t s = send(fd_client_socket, html_buf + sent, byte_read - sent, 0);
-                        if (s < 0) {
-                                fprintf(stderr, "Errore send: %d: %s\n", errno, strerror(errno));
-                                exit(EXIT_FAILURE);
-                        }
-                        sent += s;
-               }//end while
-   		break;	 
-        }
-	close(fd_HTML);
-	printf("fine invio file html");
-		
+    free(buffer);
+    return encoded;
 }
 
-void file_not_found_send_img(int fd_client_socket){
-	printf("\n invio immagine\n");
-	//invio header della risposta, con la size del corpo
-	struct stat st;	
-	char buf[256] ;
-	stat(img_for_resource_not_found, &st) ;
-	
-	snprintf(buf, 256,
-		"HTTP/1.1 404 NOT FOUND\r\n"
-		"Content-Type: image/jpeg\r\n"
-		"Content-Length: %ld\r\n"
-		"Connection: keep-alive\r\n"
-		"Cache-Control: no-cache\r\n"
-		"Date: Sun, 14 Jul 2025 10:23:45 GMT\r\n"
-		"Server: Apache/2.4.41 (Ubuntu)\r\n"
-		"\r\n", st.st_size);
-	send(fd_client_socket, buf, strlen(buf) , 0) ;
-
-	//scrittura dell'immagine	
-	int fd_failed;	
-	if((fd_failed = open(img_for_resource_not_found, O_RDONLY)) < 0){  
-		fprintf(stderr, "Errore %d: %s\n", errno, strerror(errno)) ;
-                exit(EXIT_FAILURE);
-	}
-	char * response = malloc(st.st_size * sizeof(char));	
-	int tot_byte_read = 0;
-	int byte_read = 0;
-	while((byte_read = read(fd_failed,  response + tot_byte_read,
-                        st.st_size - tot_byte_read))> 0) {
-		ssize_t sent = 0;
-		while (sent < byte_read) {
-      			ssize_t s = send(fd_client_socket, response + sent, byte_read - sent, 0);
-      			if (s < 0) {
-          			fprintf(stderr, "Errore send: %d: %s\n", errno, strerror(errno));
-            			exit(EXIT_FAILURE);
-        		}
-        		sent += s;
-			
-			if(byte_read < 0){
-				fprintf(stderr, "Errore %d: %s\n", errno, strerror(errno)) ;
-                        	exit(EXIT_FAILURE);
-				}
-		}//end while
-	}
-	
- 	printf("fine invio file img");
-}
-
-char *get_base64Img(char* path){
-	printf("\n invio file \n") ;
-
-	//apri l'immagine del file 404
-        int fd_failed;
-        if((fd_failed = open(img_for_resource_not_found, O_RDONLY)) < 0){
-                fprintf(stderr, "Errore %d: %s\n", errno, strerror(errno)) ;
-                exit(EXIT_FAILURE);
-        }
-
-	//lettura dell'immagine del file 404
-	char buf_img[4096] ;
-	int tot_byte_read = 0;
-        int byte_read     = 0;
-        while((byte_read = read(fd_failed, buf_img, sizeof(buf_img))) > 0){
-		tot_byte_read += byte_read;	
-	}
-
-	//ottenimento dell'immagine in base64
-	size_t output_len = 0 ;
-	return base64_encode(buf_img, strlen(buf_img), &output_len) ;
-	
-}
 void file_not_found_send_html_2(int fd_client_socket){
-	//invio header della risposta, con la size del corpo
-        struct stat st;
-        char buf[256] ;
-	long int header_size = 0 ;
 	
-        
-	stat(img_for_resource_not_found, &st) ;
-	header_size += st.st_size;
-	
-	stat(html_for_resource_not_found, &st) ;
-	header_size += st.st_size;
-
-	
-	snprintf(buf, 256,
-                "HTTP/1.1 404 NOT FOUND\r\n"
-                "Content-Type: image/jpeg\r\n"
-                "Content-Length: %ld\r\n"
-                "Connection: keep-alive\r\n"
-                "Cache-Control: no-cache\r\n"
-                "Date: Sun, 14 Jul 2025 10:23:45 GMT\r\n"
-                "Server: Apache/2.4.41 (Ubuntu)\r\n"
-                "\r\n", header_size);
-
-        send(fd_client_socket, buf, strlen(buf) , 0) ;
-
 	char *base64Img = get_base64Img(img_for_resource_not_found) ;
+	printf("\n base 64Img %s\n", base64Img) ;
 
 	int fdHtml = open(html_for_resource_not_found, O_RDONLY);
 	if(fdHtml < 0){
@@ -256,21 +146,56 @@ void file_not_found_send_html_2(int fd_client_socket){
         char buf_html[4096] ;
         int tot_byte_read = 0;
         int byte_read     = 0;
-        while((byte_read = read(fdHtml, buf_html, sizeof(buf_html))) > 0){
+        while((byte_read = read(fdHtml, buf_html + tot_byte_read, sizeof(buf_html))) > 0){
                 tot_byte_read += byte_read;
         }
+	buf_html[tot_byte_read] = '\0';	
 	
 	char *placeholder = strstr(buf_html, "{{placeholder}}") ;
-	int new_html_size = strlen(buf_html) - strlen("{{placeholder}}") + strlen(base64Img);
-	char *final_html = malloc(new_html_size * sizeof(char)) ;
+	if (!placeholder) {
+    		fprintf(stderr, "Placeholder non trovato\n");
+		exit(1);
+	}
+
+
+	size_t new_html_size = strlen(buf_html) - strlen("{{placeholder}}") + strlen(base64Img);
+	char *final_html = malloc(new_html_size * sizeof(char) + 1) ;
 	
 
-	int prefix_pos = placeholder - buf_html ;
+	size_t prefix_pos = placeholder - buf_html ;
+	//printf("\n prefix_pos - %ld\n", prefix_pos);
 	strncpy(final_html, buf_html, prefix_pos) ;
 	
 	strcat(final_html, base64Img);
 	strcat(final_html, placeholder + strlen("{{placeholder}}"));
 	
+	printf("\n final_html %s\n", final_html) ;	
+
+	//invio header della risposta, con la size del corpo
+        struct stat st;
+        char buf[256] ;
+	long int header_size = 0 ;	
+        
+	//stat(img_for_resource_not_found, &st) ;
+	//header_size += st.st_size;
+	
+	stat(final_html, &st) ;
+	header_size += st.st_size;
+	size_t content_length = strlen(final_html);
+
+	snprintf(buf, 256,
+                "HTTP/1.1 404 NOT FOUND\r\n"
+                "Content-Type: text/html\r\n"
+                "Content-Length: %ld\r\n"
+                "Connection: keep-alive\r\n"
+                "Cache-Control: no-cache\r\n"
+                "Date: Sun, 14 Jul 2025 10:23:45 GMT\r\n"
+                "Server: Apache/2.4.41 (Ubuntu)\r\n"
+                "\r\n", content_length);
+
+        send(fd_client_socket, buf, strlen(buf) , 0) ;
+	
+	//invio file html
 	send(fd_client_socket, final_html, strlen(final_html), 0);
 
     	free(final_html);
